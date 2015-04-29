fa22ed51657d1ceb340db27c24b383198aea1f68
elkabumi <elkabuminew@gmail.com>
2015-04-29 16:11:53 +0700

    test

---
+++ db/transaction_new_tmp.sql
@@ -0,0 +1,63 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.11
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 29 Apr 2015 pada 16.11
+-- Versi Server: 5.6.21
+-- PHP Version: 5.6.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `warung_app`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `transaction_new_tmp`
+--
+
+CREATE TABLE IF NOT EXISTS `transaction_new_tmp` (
+`tnt_id` int(11) NOT NULL,
+  `table_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `member_id` int(11) NOT NULL,
+  `menu_id` int(11) NOT NULL,
+  `tnt_price` int(11) NOT NULL,
+  `tnt_discount` int(11) NOT NULL,
+  `tnt_grand_price` int(11) NOT NULL,
+  `tnt_qty` int(11) NOT NULL,
+  `tnt_total` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `transaction_new_tmp`
+--
+ALTER TABLE `transaction_new_tmp`
+ ADD PRIMARY KEY (`tnt_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `transaction_new_tmp`
+--
+ALTER TABLE `transaction_new_tmp`
+MODIFY `tnt_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=37;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
